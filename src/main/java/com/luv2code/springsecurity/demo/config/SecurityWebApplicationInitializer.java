acdc824abca0128d1d361152f3b64ec1760df78c
Danny Romero <37481019+DannRom@users.noreply.github.com>
2021-01-20 11:25:34 -0600

    Initial commit

---
+++ src/main/java/com/luv2code/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+// This class seems to do nothing, but is actually required.
+// Even if there are no methods or variables inside of it,
+// this class is still required.
+public class SecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,18 @@
+<%--
+Created by IntelliJ IDEA.
+User: Danny
+Date: 1/18/2021
+Time: 3:01 PM
+To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>luv2code Company Home Page</title>
+</head>
+<body>
+    <h2>luv2code Company Home Page</h2>
+    <hr>
+    Welcome to the luv2code Company Home Page
+</body>
+</html>
